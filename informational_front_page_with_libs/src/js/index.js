6624d505c8e1a62796416bd950c76133aa0f27ae
Chau Nguyen <chautnguyen96@gmail.com>
2016-12-29 15:26:59 -0800

    cleaning up the menu, added scroll logic to the one with jquery, etcetc
    woops
    
    bunch of fixes to the uci sites

--- informational_front_page_with_libs/src/js/index.js
+++ informational_front_page_with_libs/src/js/index.js
@@ -1,55 +1,29 @@
 $(document).ready(function() {
-    /********* OLD LOGIC *********/
-    var body = document.querySelector('body');
-
     // Scrolling logic.
     var scrollSpeed = 1000;
 
-    document.getElementById('scroll-arrow').onclick = function() {
-        scrollIt(document.getElementById('main-nav'), scrollSpeed / 1.5);
-    };
+    $('#scroll-arrow').click(function() {
+        $('html, body').animate({ scrollTop: $('#main-nav').offset().top }, scrollSpeed);
+    });
 
-    // Source: https://pawelgrzybek.com/page-scroll-in-vanilla-javascript/
-    function scrollIt(element, duration) {
-        // define timing functions
-        var linear_easing = function(t) {
-            return t;
-        };
+    $('.top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, scrollSpeed)
+    });
 
-        // Returns document.documentElement for Chrome and Safari
-        // document.body for rest of the world
-        function checkBody() {
-            document.documentElement.scrollTop += 1;
-            var body = (document.documentElement.scrollTop !== 0) ? document.documentElement : document.body;
-            document.documentElement.scrollTop -= 1;
-            return body;
-        }
-
-        var body = checkBody();
-        var start = body.scrollTop;
-        var startTime = Date.now();
-
-        // Height checks to prevent requestAnimationFrame from infinitely looping
-        // If the function tries to scroll below the visible document area
-        // it should only scroll to the bottom of the document
-        var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-        var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-        var destination = documentHeight - element.offsetTop < windowHeight ? documentHeight - windowHeight : element.offsetTop;
-
-        function scroll() {
-            var now = Date.now();
-            var time = Math.min(1, ((now - startTime) / duration));
-            var timeFunction = linear_easing(time);
-            body.scrollTop = (timeFunction * (destination - start)) + start;
-
-            if (body.scrollTop === destination) {
-                return;
+    // Some regex stuffs I found online to take care of
+    // smooth scrolling for all the regular links.
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 40
+                }, 500);
+                return false;
             }
-            requestAnimationFrame(scroll);
         }
-        scroll();
-    }
-    /********* OLD LOGIC *********/
+    });
 
     var lastScrollPos = 0;
     var posOfNav = $('#main-nav').offset().top;
@@ -58,7 +32,6 @@ $(document).ready(function() {
         if (currentScrollPos >= posOfNav) {
             if (currentScrollPos > lastScrollPos) {
                 $('body').addClass('hide-nav');
-                $('#main-nav').removeClass('fix-top');
             } else {
                 $('body').removeClass('hide-nav');
                 $('#main-nav').addClass('fix-top');
